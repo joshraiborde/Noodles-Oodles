6b94867e205100d25330a5339aada4844ed6113f
joshsrai <joshsrai@gmail.com>
2020-12-20 15:01:32 -0500

    added has_many, reviews, noodles relationship, validates username, has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :reviewed_noodles, through: :reviews, source: :noodle
+
+    has_many :noodles #that they have created 
+
+    validates :username, uniqueness: true, presence: true
+
+    has_secure_password
+
+
 end
