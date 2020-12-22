9ce04c302217fa2a0c0b909a3424096b450224bf
joshsrai <joshsrai@gmail.com>
2020-12-22 18:00:25 -0500

    added accepts nested attributes for brand

--- app/models/noodle.rb
+++ app/models/noodle.rb
@@ -3,4 +3,7 @@ class Noodle < ApplicationRecord
   belongs_to :user #creator of it
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed it
+
+  accepts_nested_attributes_for :brand
+
 end
