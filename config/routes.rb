ff9de967300ac302a2e7ba64ac92519c6c909968
joshsrai <joshsrai@gmail.com>
2020-12-22 08:40:13 -0500

    added delete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+
 
   resources :reviews
   resources :noodles
