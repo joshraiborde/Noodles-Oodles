311add40baabd3c456bab1864faeb7e28a02a1f9
joshsrai <joshsrai@gmail.com>
2020-12-23 08:23:30 -0500

    added brand attributes method to noodle model

--- app/models/noodle.rb
+++ app/models/noodle.rb
@@ -3,7 +3,11 @@ class Noodle < ApplicationRecord
   belongs_to :user #creator of it
   has_many :reviews
   has_many :users, through: :reviews #people who have reviewed it
+  # accepts_nested_attributes_for :brand
+  
+  def brand_attributes=(attributes)
+    brand = Brand.find_or_create_by(attributes) if !attributes[:name].empty?
+  end
 
-  accepts_nested_attributes_for :brand
 
 end
