64b720a2d44e4657dcb8cd639279078e67686340
joshsrai <joshsrai@gmail.com>
2020-12-24 08:44:58 -0500

    forgot what was done here

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
 
   resources :reviews
-  resources :noodles
+  resources :noodles do
+    resources :reviews, only: [:new, :index]
+  end
   resources :brands
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
