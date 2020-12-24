0faba4ae843108282392f33fb33ae7ffaadc2b6d
joshsrai <joshsrai@gmail.com>
2020-12-24 08:36:23 -0500

    added a private current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) #find_by_id won't error out, ||= will only call the database once, 
+    end
 end
