79bd787d09421817266ce749f565e60f40a73f58
joshsrai <joshsrai@gmail.com>
2020-12-29 08:46:53 -0500

    added a not_a_duplicate validate, edited def brand_attributes, added def not_a_duplicate, added def flavor_and_brand to the noodle model

--- app/models/noodle.rb
+++ app/models/noodle.rb
@@ -5,10 +5,22 @@ class Noodle < ApplicationRecord
   has_many :users, through: :reviews #people who have reviewed it
   # accepts_nested_attributes_for :brand
 
-  validates :flavor, presence: true
+  validates :flavor, presence: true #use the plural form of validate when using default validators in the model, it is followed by the attributes that are being validated
+  validate :not_a_duplicate #use the singular form of validate when using custom validators in the model, it is followed by the sub-methods created to validate attributes
   
   def brand_attributes=(attributes)
-    brand = Brand.find_or_create_by(attributes) if !name.empty?
+    brand = Brand.find_or_create_by(attributes) if !attributes['name'].empty?
+  end
+
+  def not_a_duplicate
+    #if there is already a pack of noodles with that flavor && brand, throw an error
+    if Noodle.find_by(flavor: flavor, brand_id: brand_id)
+      errors.add(:flavor, "has already been added to that brand")
+    end
+  end
+
+  def flavor_and_brand
+    "#{flavor} - #{brand.name}"
   end
 
 
