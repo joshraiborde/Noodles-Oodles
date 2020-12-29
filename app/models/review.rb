1021a20c4e3d1a4578abe53aac2be89300e44221
joshsrai <joshsrai@gmail.com>
2020-12-29 08:48:59 -0500

    added noodle and message validator to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,7 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true , greater_than_or_equal_to: 0, less_than: 6}
+
+  validates :noodle, uniqueness: { scope: :user, message: "has already been reviewed by you"}
+
 end
