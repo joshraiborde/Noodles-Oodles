a779b441a857cddaa753b7d481bcb77a0201a376
joshsrai <joshsrai@gmail.com>
2020-12-30 09:29:20 -0500

    added a display header method to the reviews helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+    def display_header(review)
+        if params[:noodle_id]
+            content_tag(:h1, "Add a Review for #{review.noodle.flavor} -  #{review.noodle.brand.name}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+      end
 end
