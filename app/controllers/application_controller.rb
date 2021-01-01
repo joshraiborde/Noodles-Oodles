c97c5a04e5737eaf993c9c983fdbc13dde320b67
joshsrai <joshsrai@gmail.com>
2021-01-01 13:34:42 -0500

    added helpe_method to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
 
-    private
+    # private
+    helper_method :current_user
 
     def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) #find_by_id won't error out, ||= will only call the database once, 
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #find_by_id won't error out, ||= will only call the database once, 
     end
 end
