170f0fdf7dbe39250587eeaab23500897e52bf51
joshsrai <joshsrai@gmail.com>
2021-01-02 13:10:23 -0500

    added self.create by google and github omniauth methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,6 @@ class User < ApplicationRecord
       def self.create_by_github_omniauth(auth)
         self.find_or_create_by(username: auth[:info][:email]) do |u|
           u.password = SecureRandom.hex
-    
         end
       end
-
-
-
 end
